6a1ac2c6c298f0b2a175e25c77d23aad199652ba
greglo7 <gward7@mail.pima.edu>
2025-02-16 22:55:45 -0700

    cis129_lab02_shortStory.py with name and story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+# Author: Student McLearner
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+
+print('Gregory Ward') 
+print('This is my first Python lab in CIS129 course.')
+print('I want to learn code because it's intriguing and i can find a bright future in this field.')
+print('I was lost in the beginning of college but i believe this is the future i'm meant for.')
+print('Knowing code will help me out substantially and help me meet some very cool people in my journey.')
