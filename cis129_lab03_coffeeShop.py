8082d1f38afd58dcddd35b6eaab2529758501304
greglo7 <gward7@mail.pima.edu>
2025-02-23 23:34:54 -0700

    Create cis129_lab03_coffeeShop.py

---
+++ cis129_lab03_coffeeShop.py
@@ -0,0 +1,27 @@
+print("Hello, how are you?")
+print("Hi, i'm good i'd like to order 1 coffee and 2 muffins.")
+print("Would you like the muffins heated up?")
+print("Yes please.")
+print("Okay sweet, that will be $13.78. Are you paying cash or card?")
+print("Card please.")
+print("Can i get a name for the order?")
+print("Yes, Kathy")
+print("got it, we'll have that right up for you kathy.")
+
+
+
+print("*********************************")
+print("My Coffee and Muffin Shop") #restaurant name
+print("Number of coffees? 1") #how many coffees bought
+print("Number of muffins? 2") #how many muffins bought
+print("********************************")
+
+
+print("*******************************")
+print("My Coffee and Muffin Shop Receipt")
+print("1 Coffee at $5.00 each: $ 5.00") # price of coffee
+print("2 Muffins at $4.00 each: $ 8.00") # price of muffins 
+print("6% tax: $ .78")
+print("---------")
+print("Total: $ 13.78") 
+print("*********************************")
